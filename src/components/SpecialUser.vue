3eec3f87b1b5b6283eb46ff5b78f8a859aae2c03
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-13 16:25:09 -0600

    Finished the knowledge check

---
+++ src/components/SpecialUser.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 ref="user_tag"></h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        methods: {
+            handle_user_clicked(username) {
+                this.$refs[`user_tag`][`innerText`] = username;
+            }
+        },
+        mounted () {
+            this.$root.$on(`user_clicked`, this.handle_user_clicked);
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <section v-for="(username, index) in usernames" :key="index">
+      <h3 @click="highlight_username">{{ username }}</h3>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    highlight_username(details) {
+     let username = details[`target`][`innerText`];
+     this.$root.$emit(`user_clicked`, username);
+    },
+  },
+
+  data() {
+    return {
+      usernames: [`Gabriel`, `Natalia`, `Alex`, `Colleen`],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
